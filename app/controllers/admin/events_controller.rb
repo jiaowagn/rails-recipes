fe18d832661154b7a54a2ec60df48c5ac83a95e6
jiaowagn <wangjiaohere@gmail.com>
2017-06-27 23:28:44 +0800

    add category model

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -46,7 +46,7 @@ class Admin::EventsController < AdminController
   protected
 
   def event_params
-    params.require(:event).permit(:name, :description, :friendly_id, :state)
+    params.require(:event).permit(:name, :description, :friendly_id, :state, :category_id)
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,11 @@
 class Event < ApplicationRecord
+  belongs_to :category, :optional => true 
 
  validates_presence_of :name, :friendly_id
  validates_uniqueness_of :friendly_id
  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
  STATE = ["draft", "public", "private"]
- validates_inclusion_of :state, :in => STATE 
+ validates_inclusion_of :state, :in => STATE
 
  before_validation :generate_friendly_id, :on => :create
 
