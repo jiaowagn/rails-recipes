7758d71245ffe406e66612e40f2a3aef49f87dff
jiaowagn <wangjiaohere@gmail.com>
2017-06-28 22:38:47 +0800

    implement edit_admin_users_path

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,12 @@ Rails.application.routes.draw do
   namespace :admin do
     root "events#index"
     resources :events
+    resources :users 
   end
 
   get '/faq' => 'pages#faq'
 
-  resource :user 
+  resource :user
 
   root "events#index"
 
