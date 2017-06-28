3b3e2e1845482d189e2eb832af17e277c1254e1f
jiaowagn <wangjiaohere@gmail.com>
2017-06-28 23:56:07 +0800

    implement user nested profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def edit
     @user = current_user
+    @user.create_profile unless @user.profile
   end
 
   def update
@@ -16,7 +17,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:time_zone)
+    params.require(:user).permit(:time_zone, :profile_attributes => [:id, :legal_name, :birthday, :location, :education, :occupation, :bio, :specialty])
   end
 
 end
